6fd5b71c9145ecb875d974760e7ac1ff2dd326b0
Tim Caswell <tim@creationix.com>
2009-07-08 00:21:49 -0500

    Update the sample pages to have some styling to test static file downloads

---
+++ public/skins/fg.action.js
@@ -0,0 +1,23 @@
+$(function(){
+	//all hover and click logic for buttons
+	$(".fg-button:not(.ui-state-disabled)")
+	.hover(
+		function(){ 
+			$(this).addClass("ui-state-hover"); 
+		},
+		function(){ 
+			$(this).removeClass("ui-state-hover"); 
+		}
+	)
+	.mousedown(function(){
+			$(this).parents('.fg-buttonset-single:first').find(".fg-button.ui-state-active").removeClass("ui-state-active");
+			if( $(this).is('.ui-state-active.fg-button-toggleable, .fg-buttonset-multi .ui-state-active') ){ $(this).removeClass("ui-state-active"); }
+			else { $(this).addClass("ui-state-active"); }	
+	})
+	.mouseup(function(){
+		if(! $(this).is('.fg-button-toggleable, .fg-buttonset-single .fg-button,  .fg-buttonset-multi .fg-button') ){
+			$(this).removeClass("ui-state-active");
+		}
+	});
+});
+
